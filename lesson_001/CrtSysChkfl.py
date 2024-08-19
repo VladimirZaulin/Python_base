37f601377ea063b00963d8a117f64571e7875f00
Vladimir Zaulin <dream9h@gmail.com>
2024-08-19 14:12:23 +0300

    Wrote the script counting distances between cities

--- lesson_001/CrtSysChkfl.py
+++ lesson_001/CrtSysChkfl.py
@@ -5,5 +5,5 @@ info = 'OS info is \n{}\n\nPython version is {} {}' .format(
     platform.uname(), sys.version, platform.architecture())
 print(info)
 
-with open('os_info.txt', 'w') as ff:
+with open('../os_info.txt', 'w') as ff:
     ff.write(info)
