3c82b5146fee48d84ae123dbc549ae72ea997bf9
Vladimir Zaulin <dream9h@gmail.com>
2024-08-21 22:16:29 +0300

    Counted the length of some songs by a couple of methods

--- lesson_002/06_songs_list.py
+++ lesson_002/06_songs_list.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+from idlelib.config_key import GetKeysFrame
 
-# Есть список песен группы Depeche Mode со временем звучания с точносттю до долей минут
+# Есть список песен группы Depeche Mode со временем звучания с точностью до долей минут
 
 violator_songs_list = [
     ['World in My Eyes', 4.86],
@@ -19,8 +20,9 @@ violator_songs_list = [
 #   Три песни звучат ХХХ минут
 # Обратите внимание, что делать много вычислений внутри print() - плохой стиль.
 # Лучше заранее вычислить необходимое, а затем в print(xxx, yyy, zzz)
+three_songs_length_1 = violator_songs_list[3][1] + violator_songs_list[5][1] + violator_songs_list[8][1]
 
-# TODO здесь ваш код
+print('Halo, Enjoy the Silence и Clean звучат', three_songs_length_1,'мин.')
 
 # Есть словарь песен группы Depeche Mode
 violator_songs_dict = {
@@ -39,3 +41,14 @@ violator_songs_dict = {
 #   А другие три песни звучат ХХХ минут
 
 # TODO здесь ваш код
+three_songs_length_2 = violator_songs_dict['Sweetest Perfection'] + violator_songs_dict['Policy of Truth'] + violator_songs_dict['Blue Dress']
+violator_songs_dict_sum = violator_songs_dict['World in My Eyes'] + violator_songs_dict['Personal Jesus']+violator_songs_dict['Halo']+violator_songs_dict['Waiting for the Night']+ violator_songs_dict['Enjoy the Silence']+violator_songs_dict['Policy of Truth']+violator_songs_dict['Blue Dress']+ violator_songs_dict['Clean']
+left_songs_length = violator_songs_dict_sum - three_songs_length_2 - three_songs_length_1
+
+
+
+
+
+print('Sweetest Perfection, Policy of Truth и Blue Dress звучат', three_songs_length_2, 'мин.')
+print('А другие три песни звучат', left_songs_length, 'минут')
+
