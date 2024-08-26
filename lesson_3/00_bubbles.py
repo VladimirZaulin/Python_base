2f5bdff254c3c0d178666c02d4a5c1be3c75bfed
Vladimir Zaulin <dream9h@gmail.com>
2024-08-27 00:02:04 +0300

    Drew random bubbles

--- lesson_3/00_bubbles.py
+++ lesson_3/00_bubbles.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import random
 
 import simple_draw as sd
 from simple_draw import circle, get_point
@@ -7,24 +8,36 @@ sd.resolution = (1200, 600)
 
 # Нарисовать пузырек - три вложенных окружностей с шагом 5 пикселей
 
-point = get_point(200,200)
-radius = 50
-for _ in range(3):
-    radius += 5
-    sd.circle(point, radius)
 
-sd.circle(center_position=point)
+
 # Написать функцию рисования пузырька, принимающую 2 (или более) параметра: точка рисовании и шаг
-# TODO здесь ваш код
+def bubble(point,step):
+
+    radius = 50
+    for _ in range(3):
+        radius += step
+        sd.circle(point, radius, width=4)
+point = get_point(200, 200)
+bubble(point, 10)
+
 
 # Нарисовать 10 пузырьков в ряд
-# TODO здесь ваш код
+#for x in range(100,1101,100):
+  #  bubble(point, 10)
+   # point = get_point(x, 200)
 
 # Нарисовать три ряда по 10 пузырьков
-# TODO здесь ваш код
+#for y in range(1,301,100):
+ #   for x in range(100,1101,100):
+  #      bubble(point, 10)
+   #     point = get_point(x, y)
 
 # Нарисовать 100 пузырьков в произвольных местах экрана случайными цветами
-# TODO здесь ваш код
+for _ in range(100):
+    point = sd.random_point()
+    step = random.randint(1,10)
+    bubble(point,step)
+
 
 sd.pause()
 
