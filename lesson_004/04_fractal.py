fd0ccfff272a7a5df2cde50980337ef268066db6
Vladimir Zaulin <dream9h@gmail.com>
2024-09-07 17:17:44 +0300

    drew the three by funcion with rndm params

--- lesson_004/04_fractal.py
+++ lesson_004/04_fractal.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 
 import simple_draw as sd
+from simple_draw import random_number
+
+point_0 = sd.get_point(300,30)
 
 # 1) Написать функцию draw_branches, которая должна рисовать две ветви дерева из начальной точки
 # Функция должна принимать параметры:
@@ -8,6 +11,28 @@ import simple_draw as sd
 # - угол рисования,
 # - длина ветвей,
 # Отклонение ветвей от угла рисования принять 30 градусов,
+def draw_branches(point=point_0, angle=90, length=100, delta=30):
+    if length < 1:
+       return
+    v1 = sd.get_vector(point,angle,length,2)
+    v1.draw()
+    v2 = sd.get_vector(point,angle,length,2)
+    v2.draw()
+    pass
+    point = v1.end_point
+    angle = angle - delta
+    length = length * .75
+    v3 = sd.get_vector(point, angle, length, 2)
+    v3.draw()
+    pass
+    point = v2.end_point
+    angle = angle + 2 * delta - sd.random_number(5,30)
+    v4 = sd.get_vector(point, angle, length, 2)
+    v4.draw()
+    pass
+    if length > 10:
+        draw_branches(point=v3.end_point, angle=angle - sd.random_number(1,5), length = length * .75, delta=delta)
+        draw_branches(point=v4.end_point, angle=angle + sd.random_number(1,5), length = length * .75, delta=delta)
 
 # 2) Сделать draw_branches рекурсивной
 # - добавить проверку на длину ветвей, если длина меньше 10 - не рисовать
@@ -26,7 +51,7 @@ import simple_draw as sd
 
 # можно поиграть -шрифтами- цветами и углами отклонения
 
-# TODO здесь ваш код
+draw_branches()
 
 # 4) Усложненное задание (делать по желанию)
 # - сделать рандомное отклонение угла ветвей в пределах 40% от 30-ти градусов
