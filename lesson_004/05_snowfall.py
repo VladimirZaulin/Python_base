f43c1762298f2e52a7e25c44278022637a42f8cb
Vladimir Zaulin <dream9h@gmail.com>
2024-09-07 21:50:47 +0300

    Made function that make 20 snowflakes fall

--- lesson_004/05_snowfall.py
+++ lesson_004/05_snowfall.py
@@ -1,14 +1,38 @@
 # -*- coding: utf-8 -*-
+from unicodedata import numeric
 
 import simple_draw as sd
-
+from simple_draw import snowflake, random_number
 
 # На основе кода из практической части реализовать снегопад:
 # - создать списки данных для отрисовки N снежинок
 # - нарисовать падение этих N снежинок
 # - создать список рандомных длинн лучей снежинок (от 10 до 100) и пусть все снежинки будут разные
 
+
 N = 20
+
+def flying_snowflakes(length=20):
+    flakes_box = []
+    while len(flakes_box) < length:
+        flakes_box.append(sd.random_number(10,50))
+    for length in flakes_box:
+        print(length)
+        x = sd.random_number(50,750)
+        y = 450
+        while True:
+            sd.clear_screen()
+            point = sd.get_point(x, y)
+            sd.snowflake(center=point, length=length)
+            y -= 10
+            if y < 50:
+                break
+            x += 0 + sd.random_number(-5,15)
+            sd.sleep(0.1)
+            if sd.user_want_exit():
+                return
+
+flying_snowflakes()
 
 # Пригодятся функции
 # sd.get_point()
@@ -17,15 +41,9 @@ N = 20
 # sd.random_number()
 # sd.user_want_exit()
 
-# TODO здесь ваш код
-while True:
-    sd.clear_screen()
-    pass
-    pass
-    pass
-    sd.sleep(0.1)
-    if sd.user_want_exit():
-        break
+
+
+
 
 sd.pause()
 
