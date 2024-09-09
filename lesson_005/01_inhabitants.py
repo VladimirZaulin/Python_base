134f80838b4aa994eed99efbf5df46d3b4a80821
Vladimir Zaulin <dream9h@gmail.com>
2024-09-09 03:27:00 +0300

    called for needed names from other modules

--- lesson_005/01_inhabitants.py
+++ lesson_005/01_inhabitants.py
@@ -1,8 +1,15 @@
 # -*- coding: utf-8 -*-
+from pprint import pprint
 
 # Вывести на консоль жителей комнат (модули room_1 и room_2)
 # Формат: В комнате room_1 живут: ...
 
-# TODO здесь ваш код
+from room_1 import folks as folks_1
+from room_2 import folks as folks_2
+
+print("В комнате room_1 живут:", folks_1[0],'и', folks_1[1],
+    "В комнате room_2 живут:", folks_2[0])
+
+
 
 
