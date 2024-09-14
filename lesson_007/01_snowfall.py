c91f5f19a900548dfeb8c81f6ae5186848b793ad
Vladimir Zaulin <dream9h@gmail.com>
2024-09-14 14:05:02 +0300

    One SnowFlake falls, using class

--- lesson_007/01_snowfall.py
+++ lesson_007/01_snowfall.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
+from random import randint
 
 import simple_draw as sd
+
 
 # Шаг 1: Реализовать падение снежинки через класс. Внести в методы:
 #  - создание снежинки с нужными параметрами
@@ -8,20 +10,51 @@ import simple_draw as sd
 #  - отрисовку
 
 
-class Snowflake:
+class SnowFlake:
     pass
 
-    # TODO здесь ваш код
+    def __init__(self):
+        self.length = randint(20,50)
+        self.shift = randint(0,250)
+        self.color = sd.COLOR_WHITE
+        SnowFlake.quantity = 20
+        self.x = 300
+        self.y = 600
+        self.point = sd.get_point(self.x,self.y)
+        self.transparent = sd.background_color
+    def draw(self):
+        sd.start_drawing()
+        sd.snowflake(center=self.point, length=self.length, color=self.color)
+        sd.finish_drawing()
+
+    def clear_previous_picture(self):
+        sd.start_drawing()
+        sd.snowflake(center=self.point, length=self.length, color=self.transparent)
+        sd.finish_drawing()
+
+    def move(self):
+        while True:
+            self.draw()
+            self.clear_previous_picture()
+            self.y -= 10
+            self.x += 0 + sd.random_number(-10, 10)
+            self.point = sd.get_point(self.x, self.y)
+            self.draw()
+            sd.sleep(0.1)
+            sd.finish_drawing()
+            if sd.user_want_exit():
+                break
 
 
-flake = Snowflake()
+
+flake = SnowFlake()
 
 while True:
     flake.clear_previous_picture()
     flake.move()
     flake.draw()
-    if not flake.can_fall():
-        break
+    # if not flake.can_fall():
+    #     break
     sd.sleep(0.1)
     if sd.user_want_exit():
         break
