987ab44f66bbf2744886c3b3b49f8c1c9a295ce6
Vladimir Zaulin <dream9h@gmail.com>
2024-09-17 17:38:29 +0300

    Basically made a class counting letters. Sorting is needed

--- lesson_009/01_char_stat.py
+++ lesson_009/01_char_stat.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
+from fileinput import filename
+from pprint import pprint
 
-# Подсчитать статистику по буквам в романе Война и Мир.
-# Входные параметры: файл для сканирования
-# Статистику считать только для букв алфавита (см функцию .isalpha() для строк)
+# Подсчитать статистику по буквам в романе Война и Мир. +
+# Входные параметры: файл для сканирования +
+# Статистику считать только для букв алфавита (см функцию .isalpha() для строк)+
 #
 # Вывести на консоль упорядоченную статистику в виде
 # +---------+----------+
@@ -21,7 +23,48 @@
 # Упорядочивание по частоте - по убыванию. Ширину таблицы подберите по своему вкусу
 # Требования к коду: он должен быть готовым к расширению функциональности. Делать сразу на классах.
 
-# TODO здесь ваш код
+class LettersRating:
+    def __init__(self, filename):
+        self.filename = filename
+        self.stat = {}
+        self.total = None
+        self.stat_sorted = None
+
+    def get_stats(self):
+        with open(self.filename,'r', encoding='cp1251') as file: # открыли в режиме чтения
+            for line in file:
+                for char in line:
+                    if char.isalpha():
+                        if char in self.stat:
+                            self.stat[char] += 1
+                        else:
+                            self.stat[char] = 1
+                    else:
+                        pass
+        return self.stat
+    def get_total_letters(self):
+        x = self.stat.values()
+        self.total = sum(x)
+        return self.total
+
+    def show_nice_tab(self):
+
+        print('+---------+----------+')
+        print('|  буква  | частота  |')
+        print('+---------+----------+')
+        self.stat_sorted = sorted(self.stat.items(), key=lambda x: x[1])
+        dict(self.stat_sorted)
+        pprint(self.stat_sorted)
+        print('+---------+----------+')
+        print(f'| итого | {self.total} |')
+
+
+book = LettersRating('python_snippets/voyna-i-mir.txt')
+book.get_stats()
+book.get_total_letters()
+book.show_nice_tab()
+
+
 
 # После выполнения первого этапа нужно сделать упорядочивание статистики
 #  - по частоте по возрастанию
