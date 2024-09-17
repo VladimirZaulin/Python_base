135ae7bab5af0c85766822f26981d9b3c29afa43
Vladimir Zaulin <dream9h@gmail.com>
2024-09-18 01:22:27 +0300

    Different sorting orders added using classes heritage.

--- lesson_009/01_char_stat.py
+++ lesson_009/01_char_stat.py
@@ -51,6 +51,18 @@ class LettersRating:
         self.stat_sorted = sorted(self.stat.items(), key=lambda x: -x[1])
         return self.stat_sorted
 
+    def from_less_sorted(self):
+        self.stat_sorted = sorted(self.stat.items(), key=lambda x: x[1])
+        return self.stat_sorted
+
+    def from_a_sorted(self):
+        self.stat_sorted = sorted(self.stat.items(), key=lambda x: x[0])
+        return self.stat_sorted
+
+    def from_z_sorted(self):
+        self.stat_sorted = reversed(sorted(self.stat.items(), key=lambda x: x[0]))
+        return self.stat_sorted
+
     def row_maker(self):
         for a,b in self.stat_sorted:
             rem = 10 - len(str(b)) - 3
@@ -59,24 +71,83 @@ class LettersRating:
 
 
     def show_nice_tab(self):
-
+        # self.from_bigger_sorted()
         print(f'+{9*'-'}+{10*'-'}+')
         print('|  буква  | частота  |')
         print(f'+{9*'-'}+{10*'-'}+')
-        self.from_bigger_sorted()
         self.row_maker()
         # pprint(self.stat_sorted)
         print(f'+{9*'-'}+{10*'-'}+')
         print(f'|  итого  |  {self.total} |')
         print(f'+{9*'-'}+{10*'-'}+')
 
+class LettersSortedFromLess(LettersRating):
+    def show_nice_tab(self):
+        self.from_less_sorted()
+        print(f'+{9 * '-'}+{10 * '-'}+')
+        print('|  буква  | частота  |')
+        print(f'+{9 * '-'}+{10 * '-'}+')
+        self.row_maker()
+        # pprint(self.stat_sorted)
+        print(f'+{9 * '-'}+{10 * '-'}+')
+        print(f'|  итого  |  {self.total} |')
+        print(f'+{9 * '-'}+{10 * '-'}+')
 
+class LettersSortedFromA(LettersRating):
+    def show_nice_tab(self):
+        self.from_a_sorted()
+        print(f'+{9 * '-'}+{10 * '-'}+')
+        print('|  буква  | частота  |')
+        print(f'+{9 * '-'}+{10 * '-'}+')
+        self.row_maker()
+        # pprint(self.stat_sorted)
+        print(f'+{9 * '-'}+{10 * '-'}+')
+        print(f'|  итого  |  {self.total} |')
+        print(f'+{9 * '-'}+{10 * '-'}+')
+
+class LettersSortedFromZ(LettersRating):
+    def show_nice_tab(self):
+        self.from_z_sorted()
+        print(f'+{9 * '-'}+{10 * '-'}+')
+        print('|  буква  | частота  |')
+        print(f'+{9 * '-'}+{10 * '-'}+')
+        self.row_maker()
+        # pprint(self.stat_sorted)
+        print(f'+{9 * '-'}+{10 * '-'}+')
+        print(f'|  итого  |  {self.total} |')
+        print(f'+{9 * '-'}+{10 * '-'}+')
 book = LettersRating('python_snippets/voyna-i-mir.txt')
+print('Сортировка букв')
+print('от большего количества')
 book.get_stats()
+book.from_bigger_sorted()
 book.get_total_letters()
 book.show_nice_tab()
 
+book2 = LettersSortedFromLess('python_snippets/voyna-i-mir.txt')
+print('Сортировка букв')
+print('от меньшего количества')
+book2.get_stats()
+book2.from_less_sorted()
+book2.get_total_letters()
+book2.show_nice_tab()
 
+A_to_Z = LettersSortedFromA('python_snippets/voyna-i-mir.txt')
+print('Сортировка букв')
+print('в алфавитном порядке')
+A_to_Z.get_stats()
+A_to_Z.from_a_sorted()
+A_to_Z.get_total_letters()
+A_to_Z.show_nice_tab()
+
+
+Z_to_A = LettersSortedFromZ('python_snippets/voyna-i-mir.txt')
+print('Сортировка букв')
+print('в порядке обратном алфавитному')
+Z_to_A.get_stats()
+Z_to_A.from_z_sorted()
+Z_to_A.get_total_letters()
+Z_to_A.show_nice_tab()
 
 # После выполнения первого этапа нужно сделать упорядочивание статистики
 #  - по частоте по возрастанию
