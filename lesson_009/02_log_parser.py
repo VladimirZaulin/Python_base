98b9de57f3b48d24fbcbf271ee43ead4c4bbef6f
Vladimir Zaulin <dream9h@gmail.com>
2024-09-18 16:01:09 +0300

    set file output regime

--- lesson_009/02_log_parser.py
+++ lesson_009/02_log_parser.py
@@ -43,22 +43,71 @@ class LogParser:
         for items in self.counted:
             print(items, self.counted.get(items))
 
+    def file_output(self,out_file_name=None):
+        if out_file_name is not None:
+            file = open(out_file_name, 'w', encoding='utf8')
+            for key in self.counted:
+                file.write(key)
+                file.write(' ')
+                file.write(str(self.counted.get(key)))
+                file.write('\n')
+            file.close()
+        else:
+            self.form_pretty_output()
 
-parser = LogParser(file_name)
-parser.find_noks()
-parser.counter()
-parser.form_pretty_output()
 
 
+# parser = LogParser(file_name)
+# parser.find_noks()
+# parser.counter()
+# # parser.form_pretty_output()
+# parser.file_output('OutLP.txt')
 
+class ParseByHours(LogParser):
+    def find_noks(self):
+        with open(file_name,'r', encoding='cp1251') as file:
+            for line in file:
+                if 'NOK' in line:
+                    self.dates.append(line[0:14]+'h]')
+                else:
+                    pass
+        return self.dates
 
+# par_hours = ParseByHours(file_name)
+# par_hours.find_noks()
+# par_hours.counter()
+# par_hours.file_output('LPParseByHours.txt')
 
+class ParseByMonths(LogParser):
+    def find_noks(self):
+        with open(file_name,'r', encoding='cp1251') as file:
+            for line in file:
+                if 'NOK' in line:
+                    self.dates.append(line[0:8]+'m]')
+                else:
+                    pass
+        return self.dates
 
-#TODO group_by_hours
-#TODO group_by_months
-# group_by_hours = sorted(counted, key=lambda x: x[1])
-# counted = dict(counted)
-#TODO group_by_years
+# par_months = ParseByMonths(file_name)
+# par_months.find_noks()
+# par_months.counter()
+# par_months.file_output('LPParseByMonths.txt')
+
+class ParseByYear(LogParser):
+    def find_noks(self):
+        with open(file_name,'r', encoding='cp1251') as file:
+            for line in file:
+                if 'NOK' in line:
+                    self.dates.append(line[0:5]+'y]')
+                else:
+                    pass
+        return self.dates
+
+par_years = ParseByYear(file_name)
+par_years.find_noks()
+par_years.counter()
+par_years.file_output('LPParseByYear.txt')
+
 
 
 
