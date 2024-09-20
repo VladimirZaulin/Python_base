f6c17e922683ee9f70bf0588552694b262da5be5
Vladimir Zaulin <dream9h@gmail.com>
2024-09-20 22:01:41 +0300

    Tested ability to add exceptions for different Error types

---
+++ lesson_010/01_fifth_element.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+# Умножить константу BRUCE_WILLIS на пятый элемент строки, введенный пользователем
+
+BRUCE_WILLIS = 42
+try:
+    input_data = input('Если хочешь что-нибудь сделать, сделай это сам: ')
+    leeloo = int(input_data[4])
+    result = BRUCE_WILLIS * leeloo
+    print(f"- Leeloo Dallas! Multi-pass № {result}!")
+except ValueError:
+    print(f'невозможно преобразовать к числу:{ValueError}')
+except IndexError:
+    print(f'выход за границы списка(данных не достаточно):{IndexError}')
+except Exception:
+    print(f'Неизвестная ошибка{Exception}')12345
+# Ообернуть код и обработать исключительные ситуации для произвольных входных параметров
+# - ValueError - невозможно преобразовать к числу
+# - IndexError - выход за границы списка
+# - остальные исключения
+# для каждого типа исключений написать на консоль соотв. сообщение
+
+
+
+
