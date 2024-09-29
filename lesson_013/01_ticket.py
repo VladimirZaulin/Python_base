35b027a2329c9ca17937fa70b9c74b8080527253
Vladimir Zaulin <dream9h@gmail.com>
2024-09-30 00:10:21 +0300

    Avia-Ticket-forming skript ready

--- lesson_013/01_ticket.py
+++ lesson_013/01_ticket.py
@@ -11,31 +11,31 @@ from PIL import Image, ImageDraw, ImageFont, ImageColor
 
 def make_ticket(fio, from_, to, date, font_path=None):
     if font_path is None:
-        font_path = os.path.join("fonts", "ofont_ru_DS Eraser2.ttf")
+        font_path = "ofont.ru_Anticva.ttf"
     else:
         font_path = font_path
     im = Image.open("images/ticket_template.png")
     draw = ImageDraw.Draw(im)
-    font = ImageFont.truetype(font_path, size=26)
+    font = ImageFont.truetype(font_path, size=16)
 
-    y = im.size[1] - 10 - (10 + font.size) * 2
-    fio_space = f"{fio}!"
-    draw.text((10, y), fio_space, font=font, fill=ImageColor.colormap['red'])
+    y = im.size[1] - 225 - (10 + font.size) * 2
+    fio_space = f"{fio}"
+    draw.text((45, y), fio_space, font=font, fill=ImageColor.colormap['black'])
 
-    y = im.size[1] - 20 - font.size
+    y = im.size[1] - 192 - font.size
     from_space = f"{from_}"
-    draw.text((10, y), from_space, font=font, fill=ImageColor.colormap['red'])
+    draw.text((45, y), from_space, font=font, fill=ImageColor.colormap['black'])
 
-    y = im.size[1] - 10 - (10 + font.size) * 2
-    fio_space = f"{to}!"
-    draw.text((20, y), fio_space, font=font, fill=ImageColor.colormap['red'])
+    y = im.size[1] - 90 - (10 + font.size) * 2
+    fio_space = f"{to}"
+    draw.text((45, y), fio_space, font=font, fill=ImageColor.colormap['black'])
 
-    y = im.size[1] - 20 - font.size
+    y = im.size[1] - 90 - (10 + font.size) * 2
     from_space = f"{date}"
-    draw.text((20, y), from_space, font=font, fill=ImageColor.colormap['red'])
+    draw.text((265, y), from_space, font=font, fill=ImageColor.colormap['black'])
 
     # im.show()
-    out_path = 'probe.jpg'
+    out_path = 'probe.png'
     im.save(out_path)
     print(f'Ticket saved as {out_path}')
 
